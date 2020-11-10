b7b7cc437fbb7c50f35bb8bbf212e525993ca1a1
Garrett Walter <gtwalter150@gmail.com>
2020-11-10 15:37:34 -0500

    created inquirer for employee

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employee = require("./lib/Employee");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const mapMyTeam = new employee();
 
+mapMyTeam.start();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
